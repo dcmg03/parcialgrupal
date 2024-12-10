6e379ce760742d009e22bcc10cdb9ecfe552b7eb
DeisyMonroy <deisy.monroy@uptc.edu.co>
2024-12-10 18:13:52 -0500

    estructura backend

---
+++ src/main/java/com/example/parcialgrupal/ParcialGrupalApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcialgrupal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialGrupalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcialGrupalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parcialgrupal/ParcialGrupalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialgrupal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialGrupalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
